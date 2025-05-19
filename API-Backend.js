3fc8446320006634098bc1e5c47de285c29bc8d4
Mathius <issolis@estudiantec.cr>
2025-05-18 20:38:20 -0600

    [API]

--- API-Backend.js
+++ API-Backend.js
@@ -51,21 +51,26 @@ app.post("/addGender", async (req, res) => {
   }
 });
 
-app.post("/AddGenderCluster", async (req, res) => {  
+app.post("/addSubgender", async (req, res) => {  
   try {
-    const gender_cluster = req.body;
-    const response = await __DBManipulator.addGenderCluster(gender_cluster);
+    const subgender = req.body;
+    subgender['date'] = new Date().toISOString(); 
+
+
+    const response = await __DBManipulator.addSubgender(subgender);
+    
     console.log('status ', response);
 
     if (response == 200) {
-      res.status(200).send({ message: "Gender_Cluster added successfully" });
+      res.status(200).send({ message: "Gender added successfully" });
     } else {
-      res.status(400).send({ message: "Error trying to add a new Gender_Cluster" });
+      res.status(400).send({ message: "Error trying to add a new gender" });
     }
   } catch (error) {
     res.status(500).send({ message: "Server error", error: error.message });
   }
 });
+
 
 app.get("/getAllClusters", async (req, res) => {
   try {
@@ -81,6 +86,102 @@ app.get("/getAllClusters", async (req, res) => {
     res.status(500).send({ message: "Server error", error: error.message });
   }
 });
+
+
+app.get("/getAllGenders", async (req, res) => {
+  try {
+    const genders = await __DBManipulator.getGenders();
+
+    if (genders.length === 0) {
+      return res.status(404).send({ message: "No genders found" });
+    }
+
+    res.status(200).send(genders);
+
+  } catch (error) {
+    res.status(500).send({ message: "Server error", error: error.message });
+  }
+});
+
+app.get("/getAllSubgenders", async (req, res) => {
+  try {
+    const genders = await __DBManipulator.getSubgenders();
+
+    if (genders.length === 0) {
+      return res.status(404).send({ message: "No subgenders found" });
+    }
+
+    res.status(200).send(genders);
+
+  } catch (error) {
+    res.status(500).send({ message: "Server error", error: error.message });
+  }
+});
+
+app.post("/addGenderCluster", async (req, res) => {  
+  try {
+    const gender_cluster = req.body;
+    const response = await __DBManipulator.addGenderCluster(gender_cluster);
+    console.log('status ', response);
+
+    if (response == 200) {
+      res.status(200).send({ message: "Gender_Cluster added successfully" });
+    } else {
+      res.status(400).send({ message: "Error trying to add a new Gender_Cluster" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Server error", error: error.message });
+  }
+});
+
+app.post("/addGenderGender", async (req, res) => {  
+  try {
+    const gender_gender = req.body;
+    const response = await __DBManipulator.addGenderGender(gender_gender);
+    console.log('status ', response);
+
+    if (response == 200) {
+      res.status(200).send({ message: "Gender_Cluster added successfully" });
+    } else {
+      res.status(400).send({ message: "Error trying to add a new Gender_Cluster" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Server error", error: error.message });
+  }
+});
+
+app.post("/addGenderSubgender", async (req, res) => {  
+  try {
+    const gender_subgender = req.body;
+    const response = await __DBManipulator.addGenderSubgender(gender_subgender);
+    console.log('status ', response);
+
+    if (response == 200) {
+      res.status(200).send({ message: "Gender_Cluster added successfully" });
+    } else {
+      res.status(400).send({ message: "Error trying to add a new Gender_Cluster" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Server error", error: error.message });
+  }
+});
+
+app.post("/addSubgenderSubgender", async (req, res) => {  
+  try {
+    const subgender_subgender = req.body;
+    const response = await __DBManipulator.addSubgenderSubgender(subgender_subgender);
+    console.log('status ', response);
+
+    if (response == 200) {
+      res.status(200).send({ message: "Gender_Cluster added successfully" });
+    } else {
+      res.status(400).send({ message: "Error trying to add a new Gender_Cluster" });
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Server error", error: error.message });
+  }
+});
+
 
 
 
--- public/DBManipulator.js
+++ public/DBManipulator.js
@@ -16,15 +16,40 @@ export class DBManipulator {
     return result === 1 ? 200 : 400;
   }
 
-  async addGenderCluster(gender_cluster) {
-    const result = await this.manipulator.addData(this.path, gender_cluster, 'gender_cluster');
+   async addSubgender(gender) {
+    const result = await this.manipulator.addData(this.path, gender, 'subgenders');
     return result === 1 ? 200 : 400;
   }
-  
   async getClusters() {
     const clusters = await this.manipulator.getDataGroup(this.path, 'clusters');
     return clusters;
   }
+  async getGenders() {
+    const clusters = await this.manipulator.getDataGroup(this.path, 'genders');
+    return clusters;
+  }
+  async getSubgenders() {
+    const clusters = await this.manipulator.getDataGroup(this.path, 'subgenders');
+    return clusters;
+  }
 
+
+  async addGenderCluster(gender_cluster) {
+    const result = await this.manipulator.addData(this.path, gender_cluster, 'gender_cluster');
+    return result === 1 ? 200 : 400;
+  }
+  async addGenderGender(gender_gender) {
+    const result = await this.manipulator.addData(this.path, gender_gender, 'gender_gender');
+    return result === 1 ? 200 : 400;
+  }
+
+  async addGenderSubgender(gender_subgender) {
+    const result = await this.manipulator.addData(this.path, gender_subgender, 'gender_subgender');
+    return result === 1 ? 200 : 400;
+  }
+  async addSubgenderSubgender(subgender_subgender) {
+    const result = await this.manipulator.addData(this.path, subgender_subgender, 'subgender_subgender');
+    return result === 1 ? 200 : 400;
+  }
   
 }
